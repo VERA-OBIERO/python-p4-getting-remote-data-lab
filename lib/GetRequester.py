ea8cafdd2112def1d1c98b61960dda0708cd508b
VERA-OBIERO <vera.obiero@gmail.com>
2024-01-18 11:48:40 +0300

    done

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -2,12 +2,21 @@ import requests
 import json
 
 class GetRequester:
-
+    
+  
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        url="https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+        response = requests.get(url)
+        return response.content
 
     def load_json(self):
-        pass
+        return_list = []
+        persons = json.loads(self.get_response_body())
+        for person in persons:
+            return_list.append(person)
+
+        return return_list
+
